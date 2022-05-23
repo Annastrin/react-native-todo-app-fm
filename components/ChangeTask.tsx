d3a7f2eadc0abf1e6c120c54cadfd4d98ca310d7
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2022-05-23 16:07:05 +1200

    Add components for a new screen

---
+++ components/ChangeTask.tsx
@@ -0,0 +1,50 @@
+import { TextInput, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { State } from '../redux/reducers';
+import useTheme from '../hooks/useTheme';
+import { colors } from '../style-guide';
+import { changeTask } from '../redux/actions';
+
+export default function ChangeTask() {
+  const currentTask = useSelector((state: State) => state.taskToChange);
+  const [text, onChangeText] = useState(currentTask?.name);
+  const dispatch = useDispatch();
+  const theme = useTheme();
+
+  const handleSubmit = () => {
+    if (text && text.length > 0 && currentTask) {
+      dispatch(changeTask(currentTask.id, text));
+    }
+  };
+
+  return (
+    <TextInput
+      style={[styles.input, theme === 'darkTheme' && styles.inputDark]}
+      onChangeText={onChangeText}
+      onSubmitEditing={handleSubmit}
+      blurOnSubmit={true}
+      value={text}
+      multiline={true}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    marginHorizontal: 24,
+    marginVertical: 20,
+    borderWidth: 1,
+    borderColor: `${colors.lightTheme.taskSideBorders}`,
+    borderRadius: 5,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    backgroundColor: `${colors.lightTheme.taskBgColor}`,
+    color: `${colors.lightTheme.textColor}`,
+  },
+  inputDark: {
+    borderColor: `${colors.darkTheme.taskSideBorders}`,
+    backgroundColor: `${colors.darkTheme.taskBgColor}`,
+    color: `${colors.darkTheme.textColor}`,
+  },
+});
---
+++ components/TaskScreen.tsx
@@ -0,0 +1,10 @@
+import Screen from './Screen';
+import ChangeTask from './ChangeTask';
+
+export default function TaskScreen() {
+  return (
+    <Screen>
+      <ChangeTask />
+    </Screen>
+  );
+}
